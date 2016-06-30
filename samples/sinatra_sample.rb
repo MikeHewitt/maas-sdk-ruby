32f1451559a2bc1674b4e73930a5a65eba8607e7
marcisnimants <marcis.nimants@testdevlab.com>
2016-06-30 15:17:26 +0300

    add comments to sample apps code

--- samples/sinatra_sample.rb
+++ samples/sinatra_sample.rb
@@ -4,19 +4,28 @@ require 'sinatra/flash'
 require 'miracl_api'
 require 'json'
 
+# Enabling session for sinatra app and implicitly assigning it to 'session' hash
 configure do
   set :port, 5000
   enable :sessions
 end
 
 before do
+  # Retrieving credentials from 'sample.json'for authentication
   file = open("sample.json")
   json = file.read
   parsed = JSON.parse(json)
+
+  # converting parsed JSON to Ruby hash
   credentials = parsed.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+
+  # initialization of MiraclClient before every controller method
   @miracl = MiraclApi::MiraclClient.new(credentials)
 end
 
+# MiraclClient#is_authorized(session) returns access token(true) if in session or false otherwise
+# If user is not authorized then authorization url has to be obtained and passed to view
+# If user is authorized then user info can obtained by calling MiraclClient#get_email(session) and MiraclClient#get_user_id(session)
 get('/') do
   @retry = flash[:danger] ? true : false
   @is_authorized = @miracl.is_authorized(session)
@@ -29,10 +38,10 @@ get('/') do
   erb :application
 end
 
-get('/auth') do
-  redirect @miracl.get_authorization_request_url(session)
-end
-
+# When provider has authorized the user and redirects back to your app,
+# authorization has to be validated by calling MiraclClient#validate_authorization(params, session).
+# 'params' hash contains sinatra parsed request query string. It should include :state and :code so access token
+# along with user info can be obtained by calling MiraclClient#validate_authorization(params, session) and be stored in session
 get('/c2id') do
   if @miracl.validate_authorization(params, session)
     flash[:success] = "Successfully logged in!"
@@ -42,11 +51,17 @@ get('/c2id') do
   redirect '/'
 end
 
+# MiraclClient#clear_user_info(session) clears user info from session
+# but access token remains so user info can be obtained
+# by calling MiraclClient#get_email(session) and MiraclClient#get_user_id(session)
 get('/refresh') do
   @miracl.clear_user_info(session)
   redirect '/'
 end
 
+# If 'including_auth=true' is passed as second parameter to MiraclClient#clear_user_info,
+# then not only user info is cleared from session but also access token
+# so call of MiraclClient#is_authorized(session) returns false
 get('/logout') do
   @miracl.clear_user_info(session, including_auth=true)
   redirect '/'
